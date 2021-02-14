a71be5832ecb2a56f1bfce388294a219d094c382
Deep <deep.webosmotic@gmail.com>
2021-02-14 16:42:44 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(): React.ReactElement {
+    return (
+        <div>
+            something stupid
+            <p>something</p>
+        </div>
+    );
+}
+
+export default App;
